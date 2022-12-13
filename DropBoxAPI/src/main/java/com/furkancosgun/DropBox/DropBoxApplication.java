7597f10c010dd98aae7e5b33921c2cf03c83cad9
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:33:56 +0300

    Create DropBoxApplication.java

---
+++ DropBoxAPI/src/main/java/com/furkancosgun/DropBox/DropBoxApplication.java
@@ -0,0 +1,13 @@
+package com.furkancosgun.DropBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropBoxApplication.class, args);
+	}
+
+}
