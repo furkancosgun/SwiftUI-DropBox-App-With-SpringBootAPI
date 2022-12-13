84d0bb1362fe363110ce735608c0f0c6339556a0
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:33:58 +0300

    Create File.java

---
+++ DropBoxAPI/src/main/java/com/furkancosgun/DropBox/Entites/File.java
@@ -0,0 +1,35 @@
+package com.furkancosgun.DropBox.Entites;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "File")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    @Lob
+    @Column(name = "file_data", length = 999999999)
+    private byte[] fileData;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    User user;
+
+
+}
