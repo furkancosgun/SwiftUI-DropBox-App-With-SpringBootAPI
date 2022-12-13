9de8abc5730700af45605afe323aa23dad0bf88c
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:34:57 +0300

    Create DropBoxApplicationTests.java

---
+++ DropBoxAPI/src/test/java/com/furkancosgun/DropBox/DropBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkancosgun.DropBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
