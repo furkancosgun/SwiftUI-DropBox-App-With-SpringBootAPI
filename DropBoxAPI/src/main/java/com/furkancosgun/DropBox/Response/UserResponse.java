fd4494e7bad42c750dadb5399306f72b4be6a41b
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:34:12 +0300

    Create UserResponse.java

---
+++ DropBoxAPI/src/main/java/com/furkancosgun/DropBox/Response/UserResponse.java
@@ -0,0 +1,15 @@
+package com.furkancosgun.DropBox.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+    Long id;
+    String Email;
+}
