9e6dd5c7486a9c2f79c6566ae676fb56b9984bf0
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:16:11 +0300

    Create DropBoxAppApp.swift

---
+++ DropBoxApp/DropBoxApp/DropBoxAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  DropBoxAppApp.swift
+//  DropBoxApp
+//
+//  Created by Furkan on 8.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DropBoxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView{
+                ContentView()
+            }
+        }
+    }
+}
