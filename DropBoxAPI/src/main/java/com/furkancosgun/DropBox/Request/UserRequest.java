69899725875f2d2706fbd239aa7bfc02e3ac1d3f
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:34:07 +0300

    Create UserRequest.java

---
+++ DropBoxAPI/src/main/java/com/furkancosgun/DropBox/Request/UserRequest.java
@@ -0,0 +1,15 @@
+package com.furkancosgun.DropBox.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    String email;
+    String password;
+}
