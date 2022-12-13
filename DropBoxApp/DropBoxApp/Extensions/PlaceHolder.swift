6dcdeb415c0b4753eb8bf55a43dc58c7efc7555d
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:16:13 +0300

    Create PlaceHolder.swift

---
+++ DropBoxApp/DropBoxApp/Extensions/PlaceHolder.swift
@@ -0,0 +1,21 @@
+//
+//  PlaceHolder.swift
+//  DropBoxApp
+//
+//  Created by Furkan on 8.12.2022.
+//
+
+import SwiftUI
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
