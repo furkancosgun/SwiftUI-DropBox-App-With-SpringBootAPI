28f96940e105d791116ea60d3a85ab2cd1b15ffc
Furkan COSGUN <furkan51cosgun@gmail.com>
2022-12-13 22:15:44 +0300

    Create Components.swift

---
+++ DropBoxApp/DropBoxApp/Core/Components.swift
@@ -0,0 +1,20 @@
+//
+//  Components.swift
+//  DropBoxApp
+//
+//  Created by Furkan on 8.12.2022.
+//
+
+import SwiftUI
+
+struct Components: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Components_Previews: PreviewProvider {
+    static var previews: some View {
+        Components()
+    }
+}
